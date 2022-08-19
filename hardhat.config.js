c9f9d7a9542980e6182cfd4a4fcbf5d9e8b1e911
ibrahim kenia <ibrahimkenia2@gmail.com>
2022-08-19 23:42:53 +0100

    deployed in goerli network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 const { task } = require("hardhat/config");
-
+require('dotenv').config();
+const {API_URL,PRIVATE_KEY}=process.env;
 require("@nomicfoundation/hardhat-toolbox");
 task("accounts", "Prints the list of accounts", async (taskArgs,hre)=>{
   const accounts = await hre.ethers.getSigners();
@@ -16,6 +17,10 @@ module.exports = {
   networks:{
     hardhat:{
       chainId:1337
+    },
+     goerli:{
+        url:API_URL,
+        accounts:[`0x${PRIVATE_KEY}`]
     }
   },
   solidity: "0.8.4",
